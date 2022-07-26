8367bd02ab5c878a7074e10fda45aeadeeb56582
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-07-26 13:03:56 +0300

    Add changes on the home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import 'bootstrap/dist/js/bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Carousel } from 'react-bootstrap';
-import image from '../assets/images/tether.jpg';
-import rates2 from '../assets/images/main.jpg';
-import rates3 from '../assets/images/coin.jpg';
+import React from "react";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Carousel } from "react-bootstrap";
+import image from "../assets/images/tether.jpg";
+import rates2 from "../assets/images/main.jpg";
+import rates3 from "../assets/images/coin.jpg";
 
 const Home = () => (
-  <div style={{ overflow: 'none' }}>
+  <div style={{ overflow: "none" }}>
     <Carousel fade>
       <Carousel.Item>
         <img
           src={image}
           className="d-block w-100"
           alt="stock"
-          style={{ height: '100vh' }}
+          style={{ height: "100vh" }}
         />
         <Carousel.Caption>
-          <h3>Check popular stocks of varios companies 💯</h3>
-          <p>
-            We provide accurate stocks of various companies all over the world.
-          </p>
+          <h3>Check accurate coin data 💯</h3>
+          <p>We provide accurate coin data all over the world.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -28,11 +26,11 @@ const Home = () => (
           src={rates2}
           alt="stock2"
           className="d-block w-100"
-          style={{ height: '100vh' }}
+          style={{ height: "100vh" }}
         />
         <Carousel.Caption>
-          <h3>Check Gainers and losers 😇</h3>
-          <p>Avoid losses by checking out the gainers and losers of the day.</p>
+          <h3>Check data of all coins 😇</h3>
+          <p>We provide data about all coins</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -40,11 +38,11 @@ const Home = () => (
           src={rates3}
           alt="stock4"
           className="d-block w-100"
-          style={{ height: '100vh' }}
+          style={{ height: "100vh" }}
         />
         <Carousel.Caption>
-          <h3>Get accurate data of all stocks👌</h3>
-          <p>We provide all stocks data</p>
+          <h3>We record changes every minute👌</h3>
+          <p>We provide all crypto changes every minute</p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
