36e5babf2d4692728e87e2b34a31622ccb977555
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-07-26 10:59:03 +0300

    Add tests

---
+++ src/feature/searchSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const searchSlice = createSlice({
+  name: "coins",
+  initialState: "",
+  reducers: {
+    searchItem: (state, action) => action.payload,
+  },
+});
+
+export const { searchItem } = searchSlice.actions;
+
+export const selectSearchTerm = (state) => state.search;
+
+export default searchSlice.reducer;
